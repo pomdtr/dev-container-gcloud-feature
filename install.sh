527d311e2d61a6fce6ea94a03f47dedeeff3bbd6
Achille Lacoin <achille.lacoin@gmail.com>
2022-07-18 17:05:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,49 +7,11 @@ set -e
 # The tooling will parse the devcontainer-features.json + user devcontainer, and write
 # any build-time arguments into a feature-set scoped "devcontainer-features.env"
 # The author is free to source that file and use it however they would like.
+
 set -a
 . ./devcontainer-features.env
 set +a
 
-if [ "$(id -u)" -ne 0 ]; then
-    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
-    exit 1
-fi
-
-# Function to run apt-get if needed
-apt_get_update_if_needed()
-{
-    if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
-        echo "Running apt-get update..."
-        apt-get update
-    else
-        echo "Skipping apt-get update."
-    fi
-}
-
-# Checks if packages are installed and installs them if not
-check_packages() {
-    if ! dpkg -s "$@" > /dev/null 2>&1; then
-        apt_get_update_if_needed
-        apt-get -y install --no-install-recommends "$@"
-    fi
-}
-
-export DEBIAN_FRONTEND=noninteractive
-check_packages apt-transport-https ca-certificates gnupg
-
-echo "(*) Installing Gcloud CLI..."
-
-# install
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-
-if [ "${_BUILD_ARG_GCLOUD_VERSION}" != "latest" ]; then
-    echo "Installing Gcloud ${_BUILD_ARG_GCLOUD_VERSION}..."
-    apt-get -y install google-cloud-cli="${_BUILD_ARG_GCLOUD_VERSION}"
-else
-    echo "Installing latest Gcloud..."
-    apt-get update && sudo apt-get install -y google-cloud-cli
-fi
+curl https://sdk.cloud.google.com | bash
 
 echo "Done!"
